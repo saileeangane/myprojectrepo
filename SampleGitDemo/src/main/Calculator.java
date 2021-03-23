9af364e6c60cbf0d44847eb376e69318372ce7a8
Sailee Angane <Sailee Angane sailee.anane4599@gmail.com>
2021-03-23 10:53:13 +0530

    masterfun1() added

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -22,17 +22,22 @@ public class Calculator {
 	      System.out.println(num1/num2);
 	  }
 
-
 	public static void mod(int num1,int num2){
 	      System.out.println(num1%num2);
 	  }
-
 
 	public static void square(int num){
 	      System.out.println(num*num);
 	  }
 	public static void cube(int num){
 	      System.out.println(num*num*num);
+	  }
+	
+	public static void br1fun1(int num){
+	      System.out.println("In br1 with "+num);
+	  }
+	public static void masterfun1(int num){
+	      System.out.println("In master with "+num);
 	  }
 
 
